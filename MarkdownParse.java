c71e84136324c25bca84d71bfb4d2b08f92e6ef0
Catherine Gu <cgu@ucsd.edu>
2022-01-27 16:40:05 -0800

    fixed test-file4

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -22,7 +22,9 @@ public class MarkdownParse {
             if (nextOpenBracket == -1 || nextCloseBracket == -1 || openParen == -1 || closeParen == -1) {
                 break;
             }
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            if (markdown.indexOf("!") != nextOpenBracket - 1) {
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+            }
             currentIndex = closeParen + 1;
             System.out.println();
         }
