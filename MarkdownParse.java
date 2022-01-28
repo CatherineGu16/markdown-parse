65b3a53284c2c4161adbe57927efe63b7792b73d
Catherine Gu <cgu@ucsd.edu>
2022-01-27 16:35:26 -0800

    test-file4 failure

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,9 +12,10 @@ public class MarkdownParse {
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
+            /*
             if (markdown.indexOf("!") == nextOpenBracket - 1) {
                 break;
-            }
+            }*/
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
